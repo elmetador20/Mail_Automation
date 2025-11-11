68b1757314bb1b0765547f43cb64b941c0414beb
sharique ahmad <ahmedsharique250@gmail.com>
2025-11-11 11:56:52 +0530

    added error handling

--- src/main/java/com/tedx/mailer/controller/MailerController.java
+++ src/main/java/com/tedx/mailer/controller/MailerController.java
@@ -1,15 +1,20 @@
 package com.tedx.mailer.controller;
 
-import com.tedx.mailer.model.Speaker;
-
-import com.tedx.mailer.service.EmailService;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tedx.mailer.model.Speaker;
 import com.tedx.mailer.repository.SpeakerRepository;
+import com.tedx.mailer.service.EmailService;
 
 @RestController
 @RequestMapping("/api/mail")
@@ -20,6 +25,12 @@ public class MailerController {
 
     @Autowired
     private SpeakerRepository speakerRepository;
+
+    @GetMapping("/show_speakers")
+     public ResponseEntity<?> getAllSpeaker(){
+         List<Speaker> all=speakerRepository.findAll();
+        return new ResponseEntity<>(all, HttpStatus.OK);
+    }
 
     @PostMapping("/addSpeaker")
     public Speaker addSpeaker(@RequestBody Speaker speaker) {
