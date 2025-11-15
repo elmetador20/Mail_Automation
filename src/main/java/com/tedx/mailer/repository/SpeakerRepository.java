c5e9c246795d7d606e12e5102b3b231f925f9bfa
sharique ahmad <ahmedsharique250@gmail.com>
2025-11-15 19:14:56 +0530

    made changes in frontend

--- src/main/java/com/tedx/mailer/repository/SpeakerRepository.java
+++ src/main/java/com/tedx/mailer/repository/SpeakerRepository.java
@@ -1,12 +1,16 @@
 package com.tedx.mailer.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.tedx.mailer.model.Speaker;
 
-public interface SpeakerRepository extends MongoRepository<Speaker , String> {
+public interface SpeakerRepository extends MongoRepository<Speaker , String > {
     List<Speaker> findByMailStatus(String status);
+   Speaker findFirstByEmail(String email);
+   Optional<Speaker> findByEmail(String email);
+   
 
 }
